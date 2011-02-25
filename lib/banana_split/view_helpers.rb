96c487b37c9211b88049b60af8116ee2a65e514b
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2011-02-25 11:00:52 +0100

    Made the selected design sticky within the same browser session

--- lib/banana_split/view_helpers.rb
+++ lib/banana_split/view_helpers.rb
@@ -1,20 +1,26 @@
 module BananaSplit
   module ViewHelpers
-    
+
     def ab_test(test_name, designs)
       test = AbTest.find_or_create_by_name(:name => test_name.to_s)
-      
-      design_name = designs.keys.sample
-      design_render_options = designs[design_name]
-      design = AbTestDesign.find_or_create_by_ab_test_id_and_name :ab_test_id => test.id, :name => design_name.to_s
-      
-      test_run = test.runs.create({
-        :ab_test_design_id => design.id, 
+
+      test_run = AbTestRun.find_by_session_id_and_ab_test_id(session_id, test.id)
+
+      design = if test_run
+        test_run.ab_test_design
+      else
+        design_name = designs.keys.sample
+        AbTestDesign.find_or_create_by_ab_test_id_and_name(:ab_test_id => test.id, :name => design_name)
+      end
+
+      design_render_options = designs[design.name.to_sym]
+
+      test.runs.create({
+        :ab_test_design_id => design.id,
         :session_id => session_id
-      })
-      
+      }) unless test_run
+
       design_render_options ? render(design_render_options) : nil
     end
-    
   end
 end
--- lib/generators/templates/models/ab_test_run.rb
+++ lib/generators/templates/models/ab_test_run.rb
@@ -1,3 +1,4 @@
 class AbTestRun < ActiveRecord::Base
   belongs_to :ab_test
+  belongs_to :ab_test_design
 end
