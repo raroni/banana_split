61268f9825a5b4b53056d5498ed08f430208dbc4
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2011-02-25 11:29:57 +0100

    Instead of relying on just the session_id, we'll find the test_run by looking up with both session_id and test name

--- lib/banana_split/controller.rb
+++ lib/banana_split/controller.rb
@@ -9,7 +9,8 @@ module BananaSplit
     module InstanceMethods
 
       def ab_goal(test_name, value = 1)
-        test_run = AbTestRun.where(:session_id => session_id).last
+        test = AbTest.find_by_name(test_name)
+        test_run = AbTestRun.find_by_session_id_and_ab_test_id(session_id, test.id)
         if test_run && test_run.value == 0
           test_run.update_attribute(:value, value)
         end
