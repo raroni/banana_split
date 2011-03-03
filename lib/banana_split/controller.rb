db533014b6f6a75ed59432a66e2b119880c0ff8d
Rasmus Bang Grouleff <rasmusbg@virtualmanager.com>
2011-03-03 10:23:57 +0100

    If the AbTest doesn't exist, don't continue, just return

--- lib/banana_split/controller.rb
+++ lib/banana_split/controller.rb
@@ -10,6 +10,7 @@ module BananaSplit
 
       def ab_goal(test_name, value = 1)
         test = AbTest.find_by_name(test_name)
+        return if test.blank?
         test_run = AbTestRun.find_by_session_id_and_ab_test_id(session_id, test.id)
         if test_run && test_run.value == 0
           test_run.update_attribute(:value, value)
